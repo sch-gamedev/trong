0bb9ff12abb7be9e7b818f22c70dcdb9fa7fd643
siliconbrain <sir.dudas.adam@gmail.com>
2013-03-21 21:19:41 +0100

    add solution w/ sprite batch demo

---
+++ Trong/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trong
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new TrongGame())
+                game.Run();
+        }
+    }
+}
---
+++ Trong/TrongGame.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SharpDX;
+using SharpDX.Toolkit;
+using SharpDX.Toolkit.Graphics;
+
+namespace Trong
+{
+    class TrongGame : SharpDX.Toolkit.Game
+    {
+        private GraphicsDeviceManager graphicsDeviceManager;
+        private SpriteBatch spriteBatch;
+        private Texture2D testTexture;
+        private Vector2 position = Vector2.Zero;
+        private Vector2 velocity = Vector2.UnitX * 1000;
+
+        public TrongGame ()
+        {
+            graphicsDeviceManager = new GraphicsDeviceManager(this);
+
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            Window.Title = "Trong";
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            testTexture = Content.Load<Texture2D>("disc.png");
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            spriteBatch.Dispose();
+
+            base.UnloadContent();
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(testTexture, position, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (position.X + 32 > Window.ClientBounds.Width
+                || position.X < 0)
+                velocity = new Vector2(-velocity.X, velocity.Y);
+
+            position += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            base.Update(gameTime);
+        }
+    }
+}
