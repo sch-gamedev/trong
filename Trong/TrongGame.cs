afb629f8558906a17be20e610a2f453b25ed1d5d
Peter10110 <rpeter10110@gmail.com>
2013-03-25 22:34:02 +0100

    map.png nagyon fután jelenik meg

--- Trong/TrongGame.cs
+++ Trong/TrongGame.cs
@@ -13,8 +13,10 @@ namespace Trong
         private GraphicsDeviceManager graphicsDeviceManager;
         private SpriteBatch spriteBatch;
         private Texture2D testTexture;
+        private Texture2D mapTexture;
         private Vector2 position = Vector2.Zero;
         private Vector2 velocity = Vector2.UnitX * 1000;
+        private DrawingRectangle dRect;
 
         public TrongGame ()
         {
@@ -32,6 +34,8 @@ namespace Trong
         protected override void LoadContent()
         {
             testTexture = Content.Load<Texture2D>("disc.png");
+            mapTexture = Content.Load<Texture2D>("map.png");
+            dRect = new DrawingRectangle(200, 100, 300, 300);
 
             spriteBatch = new SpriteBatch(GraphicsDevice);
             base.LoadContent();
@@ -48,6 +52,7 @@ namespace Trong
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
+            spriteBatch.Draw(mapTexture, dRect, Color.White);
             spriteBatch.Draw(testTexture, position, Color.White);
             spriteBatch.End();
 
