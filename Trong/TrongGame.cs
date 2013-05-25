f4ca694f5b3cf1f35278daadf65172cde006c296
Peter10110 <rpeter10110@gmail.com>
2013-05-25 23:03:19 +0200

    Módosítottam: Disc, Ring, Player, Paddle Paddle_input alapján megcsináltam az ütők igazítását és mozgásást A labda pattog falról és ütőről, a színét az ütőnek megfelelően váltja Elkezdtem valamiféle alárendeltséget kiépíteni, nagyon félkész még
    Javítani kell:
    -a visszapattanás elég béta, és gyakran be is akad a labda
    -az ütő mozgásán finomítani kell és korlátozni kell a mozgásterét

--- Trong/TrongGame.cs
+++ Trong/TrongGame.cs
@@ -14,16 +14,19 @@ namespace Trong
         private SpriteBatch spriteBatch;
         private Ring ring;
         private InputHandler inputHandler;
-        private Player playerone;
-        private Player playertwo;
-        private Paddle paddleone;
-        private Paddle paddletwo;
+        private Player playerOne;
+        private Player playerTwo;
+        private Paddle paddleOne;
+        private Paddle paddleTwo;
         private InputContextInGame input;
+        private Disc disc;
 
 
         public TrongGame()
         {
             graphicsDeviceManager = new GraphicsDeviceManager(this);
+            graphicsDeviceManager.PreferredBackBufferWidth = 1280;
+            graphicsDeviceManager.PreferredBackBufferHeight = 720;
 
             Content.RootDirectory = "Content";
         }
@@ -33,23 +36,25 @@ namespace Trong
             Window.Title = "Trong";
 
             ring = new Ring(Window);
-
             inputHandler = new InputHandler();
-            paddleone = new Paddle(Window);
-            paddletwo = new Paddle(Window);
-            playerone = new Player(System.Windows.Forms.Keys.Space, System.Windows.Forms.Keys.Up, System.Windows.Forms.Keys.Down, paddleone);
-            playertwo = new Player(System.Windows.Forms.Keys.Shift, System.Windows.Forms.Keys.W, System.Windows.Forms.Keys.S, paddletwo);
-            input = new InputContextInGame(playerone, playertwo);
+            paddleOne = new Paddle(Window, "ring_separator.png",0);
+            paddleTwo = new Paddle(Window, "ring_separator.png",1);
+            playerOne = new Player(System.Windows.Forms.Keys.Space, System.Windows.Forms.Keys.Up, System.Windows.Forms.Keys.Down, paddleOne, "blue_disc.png");
+            playerTwo = new Player(System.Windows.Forms.Keys.Shift, System.Windows.Forms.Keys.S, System.Windows.Forms.Keys.W, paddleTwo, "blue_disc.png");
+            disc = new Disc(Window);
+            inputHandler = new InputHandler();
+            input = new InputContextInGame(playerOne, playerTwo);
+            inputHandler.ChangeContext(input);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            disc.LoadContent(Content);
             ring.LoadContent(Content);
-            paddleone.LoadContent(Content);
-            paddletwo.LoadContent(Content);
+            paddleOne.LoadContent(Content);
+            paddleTwo.LoadContent(Content);
             base.LoadContent();
         }
 
@@ -65,10 +70,10 @@ namespace Trong
             GraphicsDevice.Clear(Color.Black);
 
             spriteBatch.Begin(SpriteSortMode.Deferred, GraphicsDevice.BlendStates.NonPremultiplied);
-
+            disc.Draw(spriteBatch);
             ring.Draw(spriteBatch);
-            paddleone.Draw(spriteBatch);
-            paddletwo.Draw(spriteBatch);
+            paddleOne.Draw(spriteBatch);
+            paddleTwo.Draw(spriteBatch);
 
             spriteBatch.End();
 
@@ -78,7 +83,9 @@ namespace Trong
         protected override void Update(GameTime gameTime)
         {
             ring.Update(gameTime);
-
+            paddleOne.Update(gameTime);
+            paddleTwo.Update(gameTime);
+            disc.Update(gameTime, playerOne, playerTwo, paddleOne, paddleTwo, ring);
             base.Update(gameTime);
         }
     }
